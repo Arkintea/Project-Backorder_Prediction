aa10f99167dab523b93bf3661aca50d57b9edacd
AKINTAYO AKINPELU <AKINTAYO.AKINPELU@TRANTERIT.LOCAL>
2022-07-25 07:28:57 +0100

    data ingestion, config_entity and artifact partial update

---
+++ backorder/component/data_ingestion.py
@@ -0,0 +1,124 @@
+from backorder.entity.config_entity import DataIngestionConfig
+import  os, sys
+from backorder.exception import BackorderPredictionException
+from backorder.logger import logging
+from backorder.entity.artifact_entity import DataIngestionArtifact
+import tarfile
+import numpy as np
+from six.moves import urllib
+import pandas as pd
+from sklearn.model_selection import StratifiedShuffleSplit
+
+class DataIngestion:
+
+    def __init__(self, data_ingestion_config:DataIngestionConfig):
+        try:
+            logging.info(f"{'>>'*20}Data Ingestion log started.{'<<'*20} ")
+            self.data_ingestion_config = data_ingestion_config
+        except Exception as e:
+            raise BackorderPredictionException(e, sys)
+    
+
+    def download_data(self,) -> str:
+        try:
+            #extraction remote url to download dataset
+            download_url = self.data_ingestion_config.dataset_download_url
+
+            #folder location to download file
+            rar_download_dir = self.data_ingestion_config.rar_download_dir
+            
+            #check existence of folder
+            os.makedirs(rar_download_dir, exist_ok=True)
+
+            backorder_file_name = os.path.basename(download_url)
+
+            rar_file_path = os.path.join(rar_download_dir, backorder_file_name)
+
+            logging.info(f"Downloading file from :[{download_url}] into :[{rar_file_path}]")
+            urllib.request.urlretrieve(download_url, rar_file_path)
+            logging.info(f"File :[{rar_file_path}] has been downloaded successfully.")
+            return rar_file_path
+        except Exception as e:
+            raise BackorderPredictionException(e,sys) from e
+
+    def extract_rar_file(self, rar_file_path:str):
+        try:
+            raw_data_dir = self.data_ingestion_config.raw_data_dir
+
+            if os.path.exists(raw_data_dir):
+                os.remove(raw_data_dir)
+
+            os.makedirs(raw_data_dir, exist_ok=True)
+
+            logging.info(f"Extracting tgz file: [{rar_file_path}] into dir: [{raw_data_dir}]")
+            with tarfile.open(rar_file_path) as backorder_rar_file_obj:
+                backorder_rar_file_obj.extractall(path=raw_data_dir)
+            logging.info(f"Extraction completed")
+        except Exception as e:
+            raise BackorderPredictionException(e,sys) from e
+    
+
+    def split_data_as_train_test(self) -> DataIngestionArtifact:
+        try:
+            raw_data_dir = self.data_ingestion_config.raw_data_dir
+
+            file_name = os.listdir(raw_data_dir)[0]
+
+            backorder_file_path = os.path.join(raw_data_dir,file_name)
+
+
+
+            logging.info(f"Reading csv file: [{backorder_file_path}]")
+            backorder_data_frame = pd.read_csv(backorder_file_path)
+
+            """backorder_data_frame["went_on_backorder"] = pd.cut(backorder_data_frame["median_income"], 
+                                                            bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf], 
+                                                            labels=[1,2,3,4,5])"""            
+
+            logging.info(f"Splitting data into train and test")
+            strat_train_set = None
+            strat_test_set = None
+            split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+            for train_index, test_index in split.split(backorder_data_frame, backorder_data_frame["went_on_backorder"]):
+                strat_train_set = backorder_data_frame.loc[train_index].drop(["went_on_backorder"],axis=1)
+                strat_test_set = backorder_data_frame.loc[test_index].drop(["went_on_backorder"],axis=1)
+
+            train_file_path = os.path.join(self.data_ingestion_config.ingested_train_dir,
+                                            file_name)
+
+            test_file_path = os.path.join(self.data_ingestion_config.ingested_test_dir,
+                                        file_name)
+            
+            if strat_train_set is not None:
+                os.makedirs(self.data_ingestion_config.ingested_train_dir, exist_ok=True)
+                logging.info(f"Exporting training datset to file: [{train_file_path}]")
+                strat_train_set.to_csv(train_file_path,index=False)
+
+            if strat_test_set is not None:
+                os.makedirs(self.data_ingestion_config.ingested_test_dir, exist_ok= True)
+                logging.info(f"Exporting test dataset to file: [{test_file_path}]")
+                strat_test_set.to_csv(test_file_path,index=False)
+
+            data_ingestion_artifact = DataIngestionArtifact(train_file_path=train_file_path, 
+                                                            test_file_path=test_file_path, 
+                                                            is_ingested=True, 
+                                                            message=f"Data ingestion completed successfully.")
+            logging.info(f"Data Ingestion artifact:[{data_ingestion_artifact}]")
+            return data_ingestion_artifact
+
+        except Exception as e:
+            raise BackorderPredictionException(e,sys) from e
+
+    def initiate_data_ingestion(self)-> DataIngestionArtifact:
+        try:
+            rar_file_path =  self.download_data()
+            self.extract_rar_file(rar_file_path=rar_file_path)
+            return self.split_data_as_train_test()
+        except Exception as e:
+            raise BackorderPredictionException(e,sys) from e
+    
+
+
+    def __del__(self):
+        logging.info(f"{'>>'*20}Data Ingestion log completed.{'<<'*20} \n\n")
